261624358697fe7480f2d156d44782b525c14b52
SMK <sawmainek90@gmail.com>
2016-03-30 20:45:39 +0630

    Fix Radio Button

--- src/Mitul/Generator/FormFieldsGenerator.php
+++ src/Mitul/Generator/FormFieldsGenerator.php
@@ -113,7 +113,7 @@ class FormFieldsGenerator
 
     public static function radio($templateData, $field)
     {
-        //$textField = self::generateLabel($field);
+        $textField = "";
 
         if (count($field['typeOptions']) > 0) {
             $arr = explode(',', $field['typeOptions']);
